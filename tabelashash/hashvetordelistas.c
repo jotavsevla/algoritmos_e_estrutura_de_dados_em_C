cd91740d71a6e4910aabc548baf0066b55cfba3f
João Victor Araújo <121209009+jotavsevla@users.noreply.github.com>
2023-07-26 08:01:16 -0300

    editando e configurando Hash

--- tabelashash/hashvetordelistas.c
+++ tabelashash/hashvetordelistas.c
@@ -1,52 +1,82 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TABLE_SIZE 15
 
-typedef struct tipodado {
+typedef struct{
     int idade;
-    char* nome;
+    char nome[50];
     int cpf;
 }tipodado;
 
-typedef struct No{
+typedef struct{
     tipodado pessoa;
     struct No* prox;
-}No, Lista*;
+}No, *Lista;
 
-void inicializa_hash (Lista hash[]);
+void inicializa_hash (Lista* tabela[]);
+void inicializa_lista (Lista* nova_lista);
+No* cria_no (tipodado pessoa);
+void insere_no (No* atual);
+tipodado cria_ficha();
+void insere_tabela (Lista* tabela[], tipodado); 
+int hash (int chave);
+bool busca_na_tabela(Lista* tabela[], int idade, char* nome, int cpf);
 
-void inicializa_lista (Lista nova_lista);
-
-No* insere_no (tipodado chave, No proximo, No antec);
-
-void imprime (Lista hash[TABLE_SIZE]);
-
+void imprime (Lista* tabela[TABLE_SIZE]);
 int main (void){
-    Lista* hash = calloc (TABLE_SIZE, sizeof(Lista));
+    Lista* lista[TABLE_SIZE];
     int i;
-    inicializa_hash(&hash);
-    
-    while(i != -1){
-        
+    inicializa_hash(lista);
+    printf("Digite 1 para inserir na tabela, 2 para imprimir toda a tabela\n");
+    scanf ("%d",&i);
+    switch(i){
+        case  1: insere_tabela (lista, cria_ficha())
     }
-    imprime (&hash);
+    imprime (lista);
     return 0;
 }
-void inicializa_hash(Lista* hash){
-    for(Lista i = hash; i + TABLE_SIZE != hash + TABLE_SIZE; i++){
-        No* novo_cabeca = malloc (sizeof(No));
-        *i = novo_cabeca;
-        incializa_lista (i);
+void inicializa_hash(Lista* tabela[]){    
+    for(int i = 0; i != TABLE_SIZE; i++){
+        inicializa_lista (&tabela[i]);
     }
 }
 void inicializa_lista (Lista* nova_lista){
-    (*nova_lista)->prox = (*nova_lista);
+    *nova_lista= NULL;    // poderia utilizar o NULL tambem
 }
-insere_na_tabela(No* atual, No* prox, No* )
-void imprime (Lista hash[]){
+int hash (int chave){return chave % TABLE_SIZE;}
+No* cria_no (tipodado pessoa){
+    No* novo_no = malloc(sizeof(No));
+    novo_no->pessoa = pessoa;
+    return novo_no;
+}
+void insere_no (No* novo_no, No* atual){
+    novo_no->prox = NULL;
+    atual->prox = novo_no;
+}
+void insere_tabela (Lista* tabela[], tipodado pessoa){
+    No* percorre = tabela[hash (pessoa->idade)];
+    while (percorre != NULL)
+        percorre = percorre->prox;
+    insere_no (cria_no (pessoa), percorre);
+    
+}
+tipodado* cria_ficha (){
+    tipodado* nova_pessoa = malloc (sizeof(tipodado));
+    printf ("Digite respectivamente, Nome,Idade,CPF.\n");
+    fgets (nova_pessoa->nome);
+    while ((getchar()) != '\n');
+    scanf ("%i", nova_pessoa->idade);
+    while ((getchar()) != '\n');
+    scanf ("%i",nova_pessoa->cpf);
+
+    return nova_pessoa;
+}
+void imprime (Lista* tabela[]){
     for (int i = 0; i < TABLE_SIZE; i++){
-        printf ("posicao da tabela %i, nome %s, idade %d\n", i, hash[i]->prox.tipodado.nome, hash[i]->prox.tipodado.idade);
+        for(No* atual = tabela[i]->prox; atual != NULL; atual = atual->prox)
+            printf ("Nome: %s, Idade: %i, CPF: %i\n", atual.pessoa.nome, atual.pessoa.idade, atual.pessoa.cpf);
     }
 }

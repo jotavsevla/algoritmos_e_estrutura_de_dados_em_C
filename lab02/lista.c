c4b1dfb1ed0fc48a2cb366c3d93ea0f1bfca758c
João Victor Araújo <jotavsevla@gmail.com>
2023-08-25 12:50:47 -0300

    Lab 01 e 02 funcionais
    TADs

--- lab02/lista.c
+++ lab02/lista.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 void inicializa_lista(Lista * ap_lista){
-    Lista no_cabeca = malloc (sizeof(No));
+    (*ap_lista) = malloc (sizeof(No));
 
     (*ap_lista)->antec = (*ap_lista);
     (*ap_lista)->prox = (*ap_lista);
@@ -21,7 +21,6 @@ void insere_fim(Lista ap_lista, TipoDado valor){
     return;
 }
 void insere_inicio(Lista ap_lista, TipoDado valor){
-    
     No* novo_no = malloc (sizeof(No));
     novo_no->valor = valor;
 
@@ -34,17 +33,17 @@ void insere_inicio(Lista ap_lista, TipoDado valor){
     return;
 }
 void imprime(Lista lista){
+    printf("[");
     if (lista->prox == lista){
-        printf("Lista vazia\n");
+        printf("]\n");
         return;
     }
     No* percorre = lista->prox;
-    printf("[");
-    printf("(%i,%i)", percorre->valor.coef, percorre->valor.grau);
+    printf("(%i,%i)", percorre->valor.grau, percorre->valor.coef);
     percorre = percorre->prox;
     
     while (percorre != lista){
-        printf (",(%i,%i)",percorre->valor.coef, percorre->valor.grau);
+        printf (",(%i,%i)", percorre->valor.grau, percorre->valor.coef);
         percorre = percorre->prox;
     }
     printf ("]\n");
@@ -52,38 +51,76 @@ void imprime(Lista lista){
 }
 /* Remove valor do fim da lista apontada por ap_lista e retorna este valor. */
 TipoDado remove_fim(Lista ap_lista){
-    TipoDado tmp;
-    tmp.coef = tmp.grau = 0;
-    return tmp;
+    if (ap_lista == ap_lista->prox){
+        TipoDado vaLinexistente;
+        vaLinexistente.coef = vaLinexistente.grau = -1;
+        return vaLinexistente;
+    }
+    No* remover = ap_lista->antec;
+    TipoDado valremovido = remover->valor;
+
+    remover->antec->prox = ap_lista;
+    ap_lista->antec = remover->antec;
+    free(remover);
+
+    return valremovido;
 }
 
 /* Remove valor do inicio da lista apontada por ap_lista e retorna este valor. */
 TipoDado remove_inicio(Lista ap_lista){
-    TipoDado tmp;
-    tmp.coef = tmp.grau = 0;
-    return tmp;
+    if (ap_lista == ap_lista->prox){
+        TipoDado vaLinexistente;
+        vaLinexistente.coef = vaLinexistente.grau = -1;
+        return vaLinexistente;
+    }
+    No* remover = ap_lista->prox;
+    TipoDado valremovido = remover->valor;
+
+    remover->prox->antec = ap_lista;
+    ap_lista->prox = remover->prox;
+    free(remover);
+
+    return valremovido;
+    
 }
 
 /* Remove todas as ocorrencias de valor da lista apontada por ap_lista. 
  * Retorna o numero de ocorrencias removidas. */
 int remove_ocorrencias(Lista ap_lista, TipoDado valor){
     if (ap_lista->prox == ap_lista) return 0;
+    int count = 0;
     No* procura = ap_lista->prox;
     while(procura != ap_lista){
-        if(procura->valor.grau == valor.grau){
+        if(procura->valor.grau == valor.grau && procura->valor.coef == valor.coef){
+            count++;
             No* remove = procura;
-
+            procura = procura->prox;
+            remove->antec->prox = procura;
+            procura->antec = remove->antec;
+            procura = procura->antec;
+            free(remove);
         }
+        else
         procura = procura->prox;
     }
-    return 0;
+    if (ap_lista->prox->valor.coef == valor.coef 
+    && ap_lista->prox->valor.grau == valor.grau
+    && ap_lista->prox != ap_lista){
+        No* remove = procura;
+        procura = procura->prox;
+        remove->antec->prox = procura;
+        procura->antec = remove->antec;
+        free(remove);
+
+        return count++;
+    } 
+    return count;
 }
 
 /* Busca elemento valor na lista. 
    Retorna a posição da primeira ocorrencia de valor na lista, comecando de 0=primeira posicao.
    Retorna -1 caso nao encontrado. */
 int busca(Lista lista, TipoDado valor){
-    printf ("entrei em busca\n");
     if (lista->prox == lista) return -1;
     int count = -1;
     No* busca = lista->prox;
@@ -98,11 +135,21 @@ int busca(Lista lista, TipoDado valor){
 /* Retorna o campo coef do primeiro no que contenha grau igual ao parametro grau, 
    e 0 (zero) caso nao encontre um tal no.  */
 int coeficiente_do_grau(Lista lista, int grau){
-    return 0;
+    if (lista == lista->prox) return 0;
+    No* tmpGrau = lista->prox;
+    while (tmpGrau != lista && tmpGrau->valor.grau != grau) tmpGrau = tmpGrau->prox;
+    if(grau != tmpGrau->valor.grau) return 0;
+    return tmpGrau->valor.coef;
 }
 
 /* Desaloca toda a memória alocada da lista.
  */
 void desaloca_lista(Lista *ap_lista){
+    No* percorre = (*ap_lista)->prox;
+    while (percorre != (*ap_lista)){
+        No* remove = percorre;
+        percorre = percorre->prox;
+        free (percorre);
+    }
     return;
 }

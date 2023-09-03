59cb2f98e0a73c73165427a9be47584e0582d586
João Victor Araújo <jotavsevla@gmail.com>
2023-09-03 11:57:57 -0300

    reposição

---
+++ lab07/Arvore.c
@@ -0,0 +1,20 @@
+#include "Arvore.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+void imprime_rec(Arvore arv) {
+  if (arv == NULL) {
+    printf(".");
+    return;
+  }
+  printf("[%d:e=", arv->valor);
+  imprime_rec(arv->esq);
+  printf(",d=");
+  imprime_rec(arv->dir);
+  printf("]");
+}
+
+void imprime(Arvore arv) {
+  imprime_rec(arv);
+  printf("\n");
+}
